8e4f791ac7973c3d1f9bafe7f0d821659e58ca0d
Sole02 <ljaeseok02@gmail.com>
2026-05-27 20:34:32 +0900

    feat(cart): 장바구니 담기 API 구현

--- src/main/java/com/sparta/paymentcore/controller/CartController.java
+++ src/main/java/com/sparta/paymentcore/controller/CartController.java
@@ -2,6 +2,7 @@ package com.sparta.paymentcore.controller;
 
 import com.sparta.paymentcore.dto.AddCartItemRequest;
 import com.sparta.paymentcore.dto.AddCartItemResponse;
+import com.sparta.paymentcore.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,9 +12,10 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class CartController {
 
+    private final CartService cartService;
+
     @PostMapping("/items")
     public ResponseEntity<AddCartItemResponse> addItem(@RequestBody AddCartItemRequest request) {
         return null;
     }
-
 }
