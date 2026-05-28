95a0dd45f80e18b249dbb72629f6fb032e275b9a
Sole02 <ljaeseok02@gmail.com>
2026-05-28 14:13:34 +0900

    feat(cart): 장바구니 담기 API 구현

--- src/main/java/com/sparta/paymentcore/controller/CartController.java
+++ src/main/java/com/sparta/paymentcore/controller/CartController.java
@@ -16,6 +16,6 @@ public class CartController {
 
     @PostMapping("/items")
     public ResponseEntity<AddCartItemResponse> addItem(@RequestBody AddCartItemRequest request) {
-        return null;
+        return ResponseEntity.ok(cartService.addItem(1L, request.productId(), request.quantity()));
     }
 }
